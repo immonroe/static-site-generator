4c237923b1dc596990ffd774c7965d702b7cfd80
immonroe <immanuelmonroe@gmail.com>
2024-10-01 11:05:11 -0400

    Added test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,27 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
+    def test_text_property(self):
+        node = TextNode("Hello, world!", "bold")
+        self.assertEqual(node.text, "Hello, world!")
+
+    def test_text_type_property(self):
+        node = TextNode("Hello, world!", "italic")
+        self.assertEqual(node.text_type, "italic")
+
+    def test_url_property_none(self):
+        node = TextNode("Hello, world!", "bold")
+        self.assertIsNone(node.url)
+
+    def test_inequality(self):
+        node1 = TextNode("Hello, world!", "bold")
+        node2 = TextNode("Hello, world!", "italic")
+        self.assertNotEqual(node1, node2)
+
+    def test_url_inequality(self):
+        node1 = TextNode("Hello, world!", "bold", "https://example.com")
+        node2 = TextNode("Hello, world!", "bold")
+        self.assertNotEqual(node1, node2)
 
 if __name__ == "__main__":
     unittest.main()
